5f115155ca2a024fc2be9bb2157dfe2d422b3c53
Jack.z.deng@outlook.com <jackdeng@JackdeMacBook-Pro.local>
2021-03-24 16:46:45 +0800

    kernel.asm is created, realising the boot process

--- JavaFloppyMaker/src/OperatingSystem.java
+++ JavaFloppyMaker/src/OperatingSystem.java
@@ -9,92 +9,54 @@ import java.util.ArrayList;
 
 
 public class OperatingSystem {
-    private int[] imgContent = new int[]{
-        0xeb,0x4e,0x90,0x48,0x45,0x4c,0x4c,0x4f,0x49,0x50,0x4c,0x00,0x02,0x01,0x01,0x00,0x02,0xe0,
-        0x00,0x40,0x0b,0xf0,0x09,0x00,0x12,0x00,0x02,0x00,0x00,0x00,0x00,0x00,0x40,0x0b,0x00,0x00,0x00,0x00,0x29,
-        0xff,0xff,0xff,0xff,0x48,0x45,0x4c,0x4c,0x4f,0x2d,0x4f,0x53,0x20,0x20,0x20,0x46,0x41,0x54,0x31,0x32,
-        0x20,0x20,0x20,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0xb8,0x00,0x00,0x8e,
-        0xd0,0xbc,0x00,0x7c,0x8e,0xd8,0x8e,0xc0,0xbe,0x74,0x7c,0x8a,
-        0x04,0x83,0xc6,0x01,0x3c,0x00,0x74,0x09,0xb4,0x0e,0xbb,0x0f,0x00,0xcd,0x10,0xeb,0xee,0xf4,0xeb,0xfd
-    };
-
-    private ArrayList<Integer> imgByteToWrite = new ArrayList<Integer>();
-
-    private void readKernelFromFile(String fileName) {
-        File file = new File(fileName);
-        InputStream in = null;
-
-        try {
-            in = new FileInputStream(file);
-            int tempbyte;
-            while ((tempbyte = in.read()) != -1) {
-                imgByteToWrite.add(tempbyte);
-            }
-        } catch(IOException e) {
-            e.printStackTrace();
-            return;
-        }
-
-        imgByteToWrite.add(0x55);
-        imgByteToWrite.add(0xaa);
-        imgByteToWrite.add(0xf0);
-        imgByteToWrite.add(0xff);
-        imgByteToWrite.add(0xff);
-
+   
+    private Floppy floppyDisk = new Floppy();
+    private int  MAX_SECTOR_NUM = 18;
+    
+    
+    private void writeFileToFloppy(String fileName, boolean bootable, int cylinder,int beginSec) {
+    	File file = new File(fileName);
+    	InputStream in = null;
+    	
+    	try {
+    		in = new FileInputStream(file);
+    		byte[] buf = new byte[512];
+    		if (bootable) {
+    			buf[510] = 0x55;
+        		buf[511] = (byte) 0xaa;	
+    		}
+    		
+    		while (in.read(buf) > 0) {
+    			//将内核读入到磁盘第0面，第0柱面，第1个扇区
+    			floppyDisk.writeFloppy(Floppy.MAGNETIC_HEAD.MAGNETIC_HEAD_0, cylinder, beginSec, buf);
+    			beginSec++;
+    			
+    			if (beginSec > MAX_SECTOR_NUM) {
+    				beginSec = 1;
+    				cylinder++;
+    			}
+    		}
+    	} catch(IOException e) {
+    		e.printStackTrace();
+    		return;
+    	}
     }
-
+    
     public OperatingSystem(String s) {
-    /*  for (int i = 0; i < imgContent.length; i++) {
-            imgByteToWrite.add(imgContent[i]);
-        }
-
-        imgByteToWrite.add(0x0a);
-        imgByteToWrite.add(0x0a);
-        for (int j = 0; j < s.length(); j++) {
-            imgByteToWrite.add((int)s.charAt(j));
-        }
-        imgByteToWrite.add(0x0a);
-
-        int len = 0x1fe;
-        int curSize = imgByteToWrite.size();
-        for (int k = 0; k < len - curSize; k++) {
-            imgByteToWrite.add(0);
-        }
-
-        //0x1fe-0x1f: 0x55, 0xaa
-        //0x200-0x203: f0 ff  ff
-        imgByteToWrite.add(0x55);
-        imgByteToWrite.add(0xaa);
-        imgByteToWrite.add(0xf0);
-        imgByteToWrite.add(0xff);
-        imgByteToWrite.add(0xff);
-      */
-
-        readKernelFromFile("boot.bat");
-
-        int len = 0x168000;
-        int curSize = imgByteToWrite.size();
-        for (int l = 0; l < len - curSize; l++) {
-            imgByteToWrite.add(0);
-        }
-
+    	writeFileToFloppy(s, true, 0, 1);
     }
-
+    
+    
     public void makeFllopy()   {
-        try {
-            DataOutputStream out = new DataOutputStream(new FileOutputStream("system.img"));
-            for (int i = 0; i < imgByteToWrite.size(); i++) {
-                out.writeByte(imgByteToWrite.get(i).byteValue());
-            }
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
+    	writeFileToFloppy("kernel.bat", false, 1, 2);
+    	
+    	floppyDisk.makeFloppy("system.img");
     }
+    
+   
 
     public static void main(String[] args) {
-        OperatingSystem op = new OperatingSystem("hello, this is my first line of my operating system code");
-        op.makeFllopy();
+    	OperatingSystem op = new OperatingSystem("boot.bat");
+    	op.makeFllopy();
     }
 }
--- boot.asm
+++ boot.asm
@@ -1,4 +1,6 @@
 org 0x7c00 ; put these codes into physical address 7c00H
+
+LOAD_ADDR EQU 0x8000
 jmp entry
 
 ; just some initial steps, don't need to care
@@ -27,17 +29,25 @@ entry:
     mov ax, 0
     mov ss, ax
     mov ds, ax
-    mov si, msg
+    mov es, ax
+    mov si, ax
 
-putloop:
-    mov al, [si]
-    add si, 1
-    cmp al, 0
-    je fin
-    mov ah, 0x0e
-    mov bx, 15
-    int 0x10
-    jmp putloop
+readFloppy:
+    mov CH, 1
+    mov DH, 0
+    mov CL, 2
+
+    mov BX, LOAD_ADDR ; read the data into address: LOAD_ADDR
+    mov AH, 0x02 ; 0x02 means this is for reading
+    mov AL, 1 ; how many sector to read
+    mov DL, 0 ; the 0th floppy driver
+
+    INT 0x13 ; BIOS int, for reading floppy
+
+    JC fin ; if error, then go to fin
+
+    jmp LOAD_ADDR
+
 
 fin:
     HLT
