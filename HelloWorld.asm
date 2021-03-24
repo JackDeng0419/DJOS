0ddcd53a440dc41530d064332f6ef69ab5a2f63e
Jack.z.deng@outlook.com <jackdeng@JackdeMacBook-Pro.local>
2021-03-24 15:31:53 +0800

    the first hello world file

---
+++ HelloWorld.asm
@@ -0,0 +1,50 @@
+org 0x7c00 ; put these codes into physical address 7c00H
+jmp entry
+
+; just some initial steps, don't need to care
+db 0x90 
+DB "OSKERNEL"
+DW 512
+DB 1
+DW 1
+DB 2
+DW 224
+DW 2880
+DB 0xf0
+DW 9
+DW 18
+DW 2
+DD 0
+DD 2880
+DB 0, 0, 0x29
+DD 0xFFFFFFFF
+DB "MYFIRSTOS"
+DB "FAT12"
+RESB 18
+
+entry:
+    ; initial all segment registers
+    mov ax, 0
+    mov ss, ax
+    mov ds, ax
+    mov si, msg
+
+putloop:
+    mov al, [si]
+    add si, 1
+    cmp al, 0
+    je fin
+    mov ah, 0x0e
+    mov bx, 15
+    int 0x10
+    jmp putloop
+
+fin:
+    HLT
+    jmp fin
+
+msg:
+    DB 0x0a, 0x0a; ascii code for \n
+    DB "Hello World"
+    DB 0x0a
+    DB 0
